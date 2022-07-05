d2057d5f7d986ea7951dc6a9bcdd56fc38effb55
Tyler Snelgrove <snelgrove86@gmail>
2022-07-05 07:12:35 -0600

    First Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import request
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello"
+
+
+# Listener
+if __name__ == "__main__":
+
+    #Start the app on port 3000, it will be different once hosted
+    app.run(port=3000, debug=True)
