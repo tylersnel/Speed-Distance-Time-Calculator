61942ce065dd2abbf2094edbd028498a7d591a12
Tyler Snelgrove <snelgrove86@gmail>
2022-07-06 06:46:26 -0600

    Added distance and distance calc pages. Updated app.py

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask
-from flask import request
-app=Flask(__name__git log)
+from flask import Flask, render_template, request, redirect
+
+import os
+
+# Configuration
+
+app = Flask(__name__)
+
+# Routes 
 
 @app.route('/')
-def hello():
-    return "Hello There"
+def root():
+    return render_template("main.j2")
 
-
+@app.route('/distance', methods=["POST", "GET"])
+def distance():
+    if request.method=="POST":
+        if request.form.get ('userInput'):
+            speed=request.form['speed']
+            time=request.form['time']  
+            #data=int(speed)*int(time)
+            #return render_template("distancecalc.j2", data=data)
+    return render_template("distance.j2")
 # Listener
-if __name__ == "__main__":
 
-    #Start the app on port 3000, it will be different once hosted
-    app.run(port=3000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 65234)) 
+   
+    
+    app.run(port=port, debug=True)
