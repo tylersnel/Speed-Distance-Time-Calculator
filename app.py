621a6a9f9776e6d28feb6f6b7bdbafb42aa23b20
Tyler Snelgrove <snelgrove86@gmail>
2022-07-23 10:32:00 -0600

    Added speed calculation page.  Working time. Math is there just need to test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-
+import math
 import os
 
 # Configuration
@@ -41,6 +41,66 @@ def distance():
             data=round(int(speed)*time, 3)
             return render_template("distance.j2", data=data, measure=measure)
     return render_template("distance.j2")
+
+
+@app.route('/speed', methods=["POST", "GET"])
+def speed():
+    if request.method=="POST":
+        if request.form.get ('userInput'):
+            distance=request.form['distance']
+            time=request.form['time']
+            unit=request.form['unit']
+
+            if unit=="Kilometers":
+                measure="kph"
+            else:
+                measure="mph"
+            
+
+            #convert time that is a string to integers
+            hh=time[0]+time[1] 
+            hh=int(hh)
+            mm=time[3]+time[4]
+            mm=int(mm)
+            ss=time[6]+time[7]
+            ss=int(ss)
+
+            #convert to decimal for calculation
+            time=hh+(mm/60)+(ss/3600)
+            
+
+            data=round(int(distance)/time, 3)
+            return render_template("speed.j2", data=data, measure=measure)
+    return render_template("speed.j2")
+
+@app.route('/time', methods=["POST", "GET"])
+def time():
+    if request.method=="POST":
+        if request.form.get ('userInput'):
+            distance=request.form['distance']
+            speed=request.form['speed']
+            #convert to int for calculation
+            distance=int(distance)
+            speed=int(speed)
+
+            time=distance/speed
+
+            hours=math.trunc(time)
+
+            minutes_holder=time-hours
+            minutes_float=minutes_holder*60
+            minutes=math.trunc(minutes_float)
+
+            seconds_holder=minutes_float-minutes
+            seconds_float=seconds_holder*60
+            seconds=math.trunc(seconds_float)
+
+
+            
+
+            data=round(int(distance)/int(speed), 3)
+            return render_template("time.j2", data=data, measure=measure)
+    return render_template("time.j2")
 
 @app.route('/advanced')
 def advanced():
